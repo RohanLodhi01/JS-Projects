3353c4b2cf5400b6a756589f24c3ae778fae229f
rohanCodeLab <devrohanoist@gmail.com>
2025-12-28 12:37:48 +0530

    fix animating issues

--- 02_Foodie_hamburger/script.js
+++ 02_Foodie_hamburger/script.js
@@ -2,7 +2,12 @@ const menu = document.querySelector(".hamburger-menu-container");
 const headerContent = document.querySelector(".header-content");
 
 const closeIcon = document.querySelector(".close-icon");
+const gotoTop = document.querySelector(".go-to-top");
+const mainContent = document.querySelector(".main-content");
 
+gotoTop.addEventListener("click", () => {
+  mainContent.scrollTo(0, 0);
+});
 menu.addEventListener("click", () => {
   headerContent.classList.add("menu-open");
 });
