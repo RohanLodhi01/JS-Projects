435a2070aa993230fbc2bbe51681f411e47bd18d
RohanLodhi01 <rohanlodhi51@gmail.com>
2026-01-01 16:38:36 +0530

    resize meni icon and header

--- 02_Foodie_hamburger/script.js
+++ 02_Foodie_hamburger/script.js
@@ -1,18 +1,26 @@
-const menu = document.querySelector(".hamburger-menu-container");
+const hamburgerMenu = document.querySelector(".hamburger-menu-container");
 const headerContent = document.querySelector(".header-content");
 
 const closeIcon = document.querySelector(".close-icon");
 const gotoTop = document.querySelector(".go-to-top");
 const mainContent = document.querySelector(".main-content");
 
+const nav = document.querySelector("nav");
 gotoTop.addEventListener("click", () => {
   mainContent.scrollTo(0, 0);
 });
-menu.addEventListener("click", () => {
+nav.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+hamburgerMenu.addEventListener("click", (e) => {
+  e.stopPropagation();
   headerContent.classList.add("menu-open");
 });
 
-closeIcon.addEventListener("click", () => {
+closeIcon.addEventListener("click", (e) => {
   headerContent.classList.remove("menu-open");
 });
 
+window.addEventListener("click", () => {
+  headerContent.classList.remove("menu-open");
+});
