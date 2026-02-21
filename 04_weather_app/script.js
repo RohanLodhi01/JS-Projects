8da044673e123217750a3ac4ad54f0bab129eb2d
RohanLodhi01 <rohanlodhi51@gmail.com>
2026-02-21 19:11:16 +0530

    integrate dynamic weather image update based  on weather type

--- 04_weather_app/script.js
+++ 04_weather_app/script.js
@@ -3,10 +3,13 @@ const apiKey = "6ea68d5aa015e1618d5075c56a77ff7e";
 
 const searchBox = document.querySelector(".search-bar input");
 const searchBtn = document.querySelector(".search-bar button");
+const weatherIcon = document.querySelector(".weather-icon img");
 
 async function checkWeather(city) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`,
+    );
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -19,11 +22,27 @@ async function checkWeather(city) {
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clear"){
+      weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Clouds"){
+      weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == 'Rain'){
+      weatherIcon.src = "images/rain.png";
+    }else if(data.weather[0].main == "Drizzle"){
+      weatherIcon.src = "images/drizzle.png";
+    }else if(data.weather[0].main == "Mist"){
+      weatherIcon.src = "images/mist.png";
+    }
+
+
   } catch (error) {
     console.error("Error Fetching Weather:", error);
   }
 }
 
-searchBtn.addEventListener("click", function() {
+searchBtn.addEventListener("click", function () {
   checkWeather(searchBox.value);
 });
