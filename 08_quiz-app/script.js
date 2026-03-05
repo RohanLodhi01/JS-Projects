c3d6ea5e98b277fedd994b22567ae21d6abb0789
RohanLodhi01 <rohanlodhi51@gmail.com>
2026-03-05 18:22:28 +0530

    done ui for quiz app

---
+++ 08_quiz-app/script.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    question: "Which method is used to select an element by ID in JavaScript?",
+    options: [
+      "getElementByClass()",
+      "querySelectorAll()",
+      "getElementById()",
+      "selectById()",
+    ],
+    correctAnswer: "getElementById()",
+  },
+  {
+    question: "Which HTML tag is used to create a hyperlink?",
+    options: ["<link>", "<a>", "<href>", "<url>"],
+    correctAnswer: "<a>",
+  },
+  {
+    question: "Which CSS property is used to change text color?",
+    options: ["font-color", "text-style", "color", "background-color"],
+    correctAnswer: "color",
+  },
+  {
+    question: "How many degrees are there in a full circle?",
+    options: ["180", "360", "90", "270"],
+    correctAnswer: "360",
+  },
+];
+
+
+const 
+questions.forEach(ques, () => {
+    if(ques.correctAnswer == )
+});
